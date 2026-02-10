6bda1b9b0e7f33dedbb053473482cf1f63b7b339
PropagandalfGit <dss21@cox.net>
2026-02-09 18:07:46 -0600

    Succeeding Case

--- backend/src/main/java/com/pink/pfa/db_integration/User.java
+++ backend/src/main/java/com/pink/pfa/db_integration/User.java
@@ -21,7 +21,7 @@ public class User {
     @Column(name = "role", nullable = false)
     private String role;
 
-    private String location
+    private String location;
 
     // Constructors
     public User() {
