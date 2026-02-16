c25ae12539911e21f9bff44793d161e287c611be
Alejandro Palomino <axp22a@acu.edu>
2026-02-16 08:34:58 -0600

    ERD + V2_Database_Tables
    Pushing ER diagram and the respective SQL tables.

---
+++ backend/src/main/resources/db/migration/V2__create_pet_table.sql
@@ -0,0 +1,75 @@
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    location VARCHAR (100)
+);
+
+CREATE TABLE adoptionCenter (
+	center_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    contact_info VARCHAR(255) NOT NULL UNIQUE,
+    rating DOUBLE PRECISION,
+    location VARCHAR(100)
+);
+
+CREATE TABLE pet (
+	pet_id INT AUTO_INCREMENT PRIMARY KEY,
+    center_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    breed VARCHAR(100) NOT NULL,
+    age VARCHAR(50) NOT NULL,
+    sex VARCHAR(10) NOT NULL,
+    pet_type VARCHAR(50) NOT NULL,
+    price DOUBLE NOT NULL,
+    pet_status VARCHAR(50) NOT NULL,
+    compatibility_score INT,
+    FOREIGN KEY (center_id) REFERENCES adoptionCenter(center_id)
+);
+
+CREATE TABLE featuredPets (
+	pet_id INT AUTO_INCREMENT PRIMARY KEY,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    reason VARCHAR(255) NOT NULL,
+    FOREIGN KEY (pet_id) REFERENCES pet(pet_id)
+);
+
+CREATE TABLE petRating (
+	rating_id DOUBLE PRECISION AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    pet_id INT NOT NULL,
+    rating_date DATE NOT NULL,
+    rating DOUBLE PRECISION NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (pet_id) REFERENCES pet(pet_id)
+);
+
+CREATE TABLE reviews (
+	review_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    center_id INT NOT NULL,
+    rating DOUBLE PRECISION NOT NULL,
+    rt_comment TEXT NOT NULL,
+    rt_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (center_id) REFERENCES adoptionCenter(center_id)
+);
+
+CREATE TABLE comments (
+	comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    ct_date DATE NOT NULL,
+    ct_comment TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE submissions (
+	submission_id INT AUTO_INCREMENT PRIMARY KEY,
+    contributor_id INT NOT NULL,
+    pet_info VARCHAR(255) NOT NULL,
+    pet_status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (contributor_id) REFERENCES user(user_id)
+);
