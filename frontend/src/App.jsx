e557d4435f1d5090d51626bcfdad4ea0deda446d
Grant1818 <tgh22b@acu.edu>
2026-02-20 09:07:28 -0600

    fix done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from './pages/login.jsx'
 import Signup from './pages/signup.jsx'
 import Home from './pages/home.jsx'
 import SelectAnimal from './pages/SelectAnimal.jsx'
+import AnimalFilter from './pages/AnimalFilter.jsx'
 import AboutUs from './pages/AboutUs.jsx'
 import Help from './pages/help.jsx'
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
         <Route path="/select-animal" element={<SelectAnimal />} />
+        <Route path="/animal-filter" element={<AnimalFilter />} />
         <Route path="/About" element={<AboutUs />} />
         <Route path="/help" element={<Help />} />
       </Routes>
