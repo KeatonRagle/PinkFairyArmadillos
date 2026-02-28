e5cd9a9d6a689aff129f838302d1c522a7519ddc
Austin <austin09017@gmail.com>
2026-02-28 12:48:34 -0600

    created necessary files and still doing research

--- backend/src/main/java/com/pink/pfa/PfaApplication.java
+++ backend/src/main/java/com/pink/pfa/PfaApplication.java
@@ -2,6 +2,7 @@ package com.pink.pfa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 /**
@@ -27,6 +28,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * and begins listening for incoming API requests.
  */
 @SpringBootApplication
+@EnableScheduling
 public class PfaApplication {
 	/**
 	 * Main method used to launch the Spring Boot application.
---
+++ backend/src/main/java/com/pink/pfa/schedulers/WebScraperScheduler.java
@@ -0,0 +1,9 @@
+package com.pink.pfa.schedulers;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebScraperScheduler {
+    
+    
+}
