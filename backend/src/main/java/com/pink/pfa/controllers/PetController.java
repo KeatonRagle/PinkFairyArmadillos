de4f4248292c0303b5629733386412d2ec361b79
PropagandalfGit <dss21@cox.net>
2026-04-10 01:37:08 -0500

    Added quick pet post endpoint

--- backend/src/main/java/com/pink/pfa/controllers/PetController.java
+++ backend/src/main/java/com/pink/pfa/controllers/PetController.java
@@ -2,15 +2,22 @@ package com.pink.pfa.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pink.pfa.controllers.requests.PetRequest;
+import com.pink.pfa.controllers.requests.PostRequest;
 import com.pink.pfa.exceptions.ResourceNotFoundException;
+import com.pink.pfa.exceptions.SiteAlreadyExistsException;
 import com.pink.pfa.models.datatransfer.PetDTO;
+import com.pink.pfa.models.datatransfer.PostDTO;
 import com.pink.pfa.services.PetService;
 
 /**
@@ -89,6 +96,19 @@ public class PetController {
             return ResponseEntity.ok().body(petService.findById(id));
         } catch (ResourceNotFoundException e) {
             return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping("/addPet")
+    public ResponseEntity<PetDTO> addPet(
+        @RequestBody PetRequest petReq
+    ) {
+        try { 
+            return ResponseEntity.ok().body(petService.addPet(petReq));
+        } catch (SiteAlreadyExistsException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
